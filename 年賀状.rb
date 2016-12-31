152471e16235053171103220ad41b43d028f7c35
S-bite <0125bitbite@gmail.com>
2016-12-31 10:22:37 +0900

    年賀状型プログラム

---
+++ 年賀状.rb
@@ -0,0 +1,20 @@
+
+
+#あけましておめでとうございます
+
+	
+        eval(%w!"xv7    eon64wl5ruya    jvid    esgb7h7uis9j
+        em1r2unn39bf    yie1lsb4voip    a6jw    xopwlvaexdiz
+                pr1q    o45k    xg12    qch7    7mn2    g7k0
+                081s    5wb8    j31s    en72    au7r    kn2o
+        5cuwxrceyi31    7adk    hox7    koew            vtx4
+        94fdm78vi65a    raly    1ccv    anc3            4yuv
+        m".t            o_i(    36).    to_s            (2). 
+        gsub            ("01    0","    \n")            .gsu
+        b("0",32.chr    ).gsub("1","    #").            char
+        s{|c|print(c    );sleep(0.01    );}!            *"")
+
+
+#                                              年もよろしくお願いします。
+
+#                                                      goo.gl/Ot9lSQ
